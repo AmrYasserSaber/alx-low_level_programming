98a2d2551b5bb66ca84ed7b5607a8c19bcbd97d3
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-03-21 14:23:30 +0200

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *print_last_digit - print the last digit
+ * @n : takes integer
+ * Return:void
+ */
+
+void print_last_digit(int n)
+{
+	printf("%d", n % 10);
+}
