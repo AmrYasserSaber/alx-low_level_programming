9fff46c0c1429c8b177b41a1d70979264b183a2f
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-03-21 13:40:07 +0200

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *_isalpha - check if its a letter.
+ *@c: the character to check
+ * Return: 1 or 0.
+ */
+
+int _isalpha(char c)
+{
+return (c >= 97 && c <= 122 || c >= 65 && c <= 90);
+}
+
