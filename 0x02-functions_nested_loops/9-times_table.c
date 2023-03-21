728c9f658b1c885ad08ece52e391132cb19f692a
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-03-21 15:47:21 +0200

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,12 +17,16 @@ void times_table(void)
 		{
 			if (j == 0)
 			{
-				printf(" %d\n", i * j);
+				printf(" %d,", i * j);
 			}
 			else
 			{
 				printf(" %3d,", i * j);
 			}
 		};
+		printf("\n");
 	}
+}
+int main(void){
+	times_table();
 }
