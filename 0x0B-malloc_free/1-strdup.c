9fe49dc6549cf3de91aa99ccad9c3b97dcda3329
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-04-11 16:42:46 +0200

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int strlen(char *str);
+
+/**
+ * strlen - get the length of string.
+ * @str: the string.
+ *
+ * Return: intrger of the length.
+ */
+
+int strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+
+
+
+/**
+ * _strdup - creates a space and put the string in it.
+ * @str: the string.
+ *
+ * Return: string.
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+
+	char *my_array = malloc(strlen(str) * sizeof(char));
+
+	if (my_array == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		my_array[i] = str[i];
+		i++;
+	}
+
+	*(my_array + i) = '\0';
+
+	return (my_array);
+}
