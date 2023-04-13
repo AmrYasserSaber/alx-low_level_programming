64215fc89227d78b352b71b25f004da51697bca4
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-04-13 16:30:10 +0200

    initalize

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,46 +2,44 @@
 #include "main.h"
 
 /**
- * string_nconcat - concat two strings
- * @s1:first string
- * @s2:second string
- * @n:number of characters from string 2
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
  *
- * Return: a pointer to the concatnated string
+ * Return: pointer to the resulting string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int j = 0,len1 = 0,len2 = 0;
-
-	char *my_array;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
-	{
 		len1++;
-	}
-
 	while (s2 && s2[len2])
-	{
 		len2++;
-	}
 
-	if (n > len2)
-		n = len2;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	my_array = malloc(len1 + n + 1);
-
-	if (!my_array)
+	if (!s)
 		return (NULL);
 
-	for (int i = 0 ; i < len1 ;  i++)
-		my_array[i] = s1[i];
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
 
-	for (j = 0; j < n ; j++)
-		my_array[len1 + j] = s2[j];
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	my_array[len1 + j] = '\0';
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	return (my_array);
+	s[i] = '\0';
 
+	return (s);
 }
