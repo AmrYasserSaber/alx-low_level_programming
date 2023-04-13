d6bb2008522023208849f6a4ab106072667d1a1d
AmrYasserSaber <amr.yassr.2@gmail.com>
2023-04-13 17:05:48 +0200

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	*my_array;
+	char *my_array;
 	unsigned int i = 0;
 
 	my_array = malloc(size * sizeof(nmemb));
