12ccef846affb4bfe6229475f18bb4cdf3f4eb9e
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-04-17 21:59:00 +0200

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef Dog_H
+#define Dog_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
