9f4b49d3ad1673feea937aa191768395e117dc00
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-04-19 17:56:38 +0200

    README

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,10 +12,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+    unsigned int j;
+
 	if (!array || !action)
 		return;
-	for (unsigned int i = 0; i < size; i++)
+
+	for (j = 0; j < size; j++)
 	{
-		action(array[i]);
+		action(array[j]);
 	}
 }
