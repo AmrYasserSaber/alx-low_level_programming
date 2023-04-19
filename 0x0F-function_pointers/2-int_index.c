9e58f94883a4ade24db3ad3e35e457d290052528
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-04-19 18:10:46 +0200

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  int_index-function pointer that run a function on each element of array
+ * @array: the aaray we want to iterate on
+ * @size: the size of the array
+ * @cmp: pointer to function that compar the value
+ * Return: Nothing.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+}
