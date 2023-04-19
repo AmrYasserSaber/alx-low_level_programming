3938e31474a329918c03c6b3a7d9282c1039a6d6
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-04-19 17:50:47 +0200

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (!array || !size || !action)
+	if (!array || !action)
 		return;
 	for (unsigned int i = 0; i < size; i++)
 	{
