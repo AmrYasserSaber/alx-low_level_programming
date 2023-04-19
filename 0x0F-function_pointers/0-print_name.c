043754700f093a574afa2b889abe8a83c7852adc
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-04-19 17:00:36 +0200

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,9 @@
 #include "function_pointers.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
  * print_name -prints a name acording to the function passed in the parameters
  * @name: name of the person
- * @f : a pointer function
- *
+ * @f: pointer to the function
  * Return: Nothing.
  */
 
